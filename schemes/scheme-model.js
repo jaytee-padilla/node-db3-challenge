2b28abf51e1324fda3fd68aa936f5220f101f3fe
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-15 00:16:23 -0700

    Updated 02-steps.js seed with correct step_numbers. Added ability to get steps by id from steps table

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -21,7 +21,11 @@ function findById(id) {
 }
 
 function findSteps(id) {
-
+	return db('schemes')
+		.join('steps', 'schemes.id', '=', 'steps.scheme_id')
+		.select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+		.where('schemes.id', id)
+		.orderBy('steps.step_number');
 }
 
 function add(scheme) {
